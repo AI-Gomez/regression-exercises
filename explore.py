1d323cc1c31a5f4771f88c536bc56fd7c8874c72
Angel Gomez <gomez.i.angel3@gmail.com>
2020-10-01 08:42:38 -0500

    updt

--- explore.py
+++ explore.py
@@ -6,33 +6,29 @@ import wrangle
 from env import host, user, password
 import seaborn as sns
 import matplotlib.pyplot as plt
+import math
 
-def plot_variable_pairs():
-    df = wrangle.get_data_from_sql()
-    train_and_validate, test = train_test_split(df, test_size=.2, random_state=123)
-    train, validate = train_test_split(train_and_validate, test_size=.2, random_state=123)
-    fig = sns.regplot(x='tenure', y='monthly_charges', data=train)
-    return fig
-
-def months_to_years():
-    df = wrangle.get_data_from_sql()
-    df['tenure_years'] = (df.tenure / 12).round(0)
+def plot_variable_pairs(df, drop_scaled_columns=True):
+    if drop_scaled_columns:
+        # drop the columns that have _scaled in their name
+        scaled_columns = [c for c in df.columns if c.endswith('_scaled')]
+        df = df.drop(columns=scaled_columns)
     
-    return df
-
-def plot_categorical_and_continuous_vars():
-    df = wrangle.get_data_from_sql()
-    df['tenure_years'] = (df.tenure / 12).round(0)
-    train_and_validate, test = train_test_split(df, test_size=.2, random_state=123)
-    train, validate = train_test_split(train_and_validate, test_size=.2, random_state=123)
+    g = sns.PairGrid(df)
+    g.map_offdiag(sns.regplot)
+    g.map_diag(plt.hist)
     
-    plt.subplot(321)
-    f1 = sns.boxplot(y='monthly_charges', x='tenure_years', data=train)
-    plt.subplot(322)
-    f2 = train.tenure_years.value_counts().sort_index().plot.bar()
-    plt.subplot(323)
-    f3 = sns.swarmplot(data=train, y='monthly_charges', x='tenure_years')
-    plt.subplot(324)
-    f4 = sns.violinplot(data=train, y='monthly_charges', x='tenure_years')
+    return g
+
+def months_to_years(df):
+    return df.assign(tenure_years=(df.tenure / 12).apply(math.floor))
+    
+
+def plot_categorical_and_continuous_vars(df, categorical_var, continuous_var):
+    sns.barplot(data=df, y=continuous_var, x=categorical_var)
+    plt.show()
+    sns.violinplot(data=df, y=continuous_var, x=categorical_var)
+    plt.show()
+    sns.boxplot(data=df, y=continuous_var, x=categorical_var)
 
     
